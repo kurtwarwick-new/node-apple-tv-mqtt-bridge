27f3b5b399dc80150e5a91a5e915ef556e8fbc15
Kurt Warwick <kurtw@mda.local>
2020-05-27 22:54:40 +0200

    fixes

--- bin/cli.js
+++ bin/cli.js
@@ -1,27 +1,16 @@
 #!/usr/bin/env node
 
 try {
-const fs = require("fs");
-const path = require("path");
-const minimist = require('minimist');
-const application = require("../src");
+    const fs = require("fs");
+    const path = require("path");
+    const minimist = require("minimist");
+    const application = require("../src");
+    const args = minimist(process.argv.splice(2));
+    const configFilePath = path.resolve(process.cwd(), args.config);
 
-const args = minimist(process.argv.splice(2));
+    let config = require(configFilePath);
 
-const configFilePath = path.resolve(process.cwd(), args.config);
-
-let config = require(configFilePath);
-
-console.log(config);
-
-application(config);
-
-//const args = process.args.split("--").map(arg => );
-//const configFilePath = process.args[]
-
-//let config = require(process.args[0]);
-
-//appleTvMqttBridge(config);
+    application(config);
 } catch (error) {
     console.log(error);
 }
